4d5ab3c2746a7883922e777800920eaf6698f5a7
ru39391 <dev@39391.ru>
2022-09-02 22:08:11 +0300

    feature/настроить скроллер промо-блока

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import '../scss/main.scss';
 
 import './app/nav';
+import './app/promo';
 import './app/reviews';
--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -1,6 +1,7 @@
 export const scrollerConfig = {
   scrollerBtnPrevSelector: '.swiper-nav__item_dir_prev',
-  scrollerBtnNextSelector: '.swiper-nav__item_dir_next'
+  scrollerBtnNextSelector: '.swiper-nav__item_dir_next',
+  scrollerPagination: '.swiper-pagination'
 }
 
 export function toggleClassName(togglerEl, {targetSel, classNameActive}) {
