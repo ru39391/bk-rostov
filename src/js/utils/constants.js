62ebf46a2bf3151be43377008f2e0b53ee97877d
ru39391 <dev@39391.ru>
2022-09-02 14:22:28 +0300

    feature/создать карусель отзывов

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -1,3 +1,8 @@
+export const scrollerConfig = {
+  scrollerBtnPrevSelector: '.swiper-nav__item_dir_prev',
+  scrollerBtnNextSelector: '.swiper-nav__item_dir_next'
+}
+
 export function toggleClassName(togglerEl, {targetSel, classNameActive}) {
   const el = {
     target: togglerEl.closest(targetSel),
